a373f8db14ca298bcb90971025884641bf83df23
Xiao_Yihong <1065858439@qq.com>
2024-05-14 15:55:08 +0800

    Add files via upload

---
+++ datasets/ReID/make_model_clipreid.py
@@ -0,0 +1,79 @@
+import torch
+from torch import nn
+
+from datasets.ReID.clip import clip
+
+
+class PromptLearner(nn.Module):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    def __init__(self, num_class, dataset_name, dtype, token_embedding):
+        super().__init__()
+        if dataset_name == "VehicleID" or dataset_name == "veri":
+            ctx_init = "A photo of a X X X X vehicle."
+        else:
+            ctx_init = "A photo of a X X X X person."
+
+        ctx_dim = 512
+        # use given words to initialize context vectors
+        ctx_init = ctx_init.replace("_", " ")
+        n_ctx = 4
+
+        tokenized_prompts = clip.tokenize(ctx_init)
+        with torch.no_grad():
+            embedding = token_embedding(tokenized_prompts).type(dtype)
+        self.tokenized_prompts = tokenized_prompts  # torch.Tensor
+
+        n_cls_ctx = 4
+        cls_vectors = torch.empty(num_class, n_cls_ctx, ctx_dim, dtype=dtype)
+        nn.init.normal_(cls_vectors, std=0.02)
+        self.cls_ctx = nn.Parameter(cls_vectors)
+
+        # These token vectors will be saved when in save_model(),
+        # but they should be ignored in load_model() as we want to use
+        # those computed using the current class names
+        self.register_buffer("token_prefix", embedding[:, :n_ctx + 1, :])
+        self.register_buffer("token_suffix", embedding[:, n_ctx + 1 + n_cls_ctx:, :])
+        self.num_class = num_class
+        self.n_cls_ctx = n_cls_ctx
+
+    def forward(self, label):
+        cls_ctx = self.cls_ctx[label]
+        b = label.shape[0]
+        prefix = self.token_prefix.expand(b, -1, -1)
+        suffix = self.token_suffix.expand(b, -1, -1)
+
+        prompts = torch.cat(
+            [
+                prefix,  # (n_cls, 1, dim)
+                cls_ctx,  # (n_cls, n_ctx, dim)
+                suffix,  # (n_cls, *, dim)
+            ],
+            dim=1,
+        )
+
+        return prompts
+
+
+class TextEncoder(nn.Module):
+    def __init__(self, base_model):
+        super().__init__()
+        self.transformer = base_model.transformer
+        self.positional_embedding = base_model.positional_embedding
+        self.ln_final = base_model.ln_final
+        self.text_projection = base_model.text_projection
+        self.dtype = base_model.dtype
+
+    def forward(self, prompts, tokenized_prompts):
+        x = prompts.type(self.dtype) + self.positional_embedding.type(self.dtype)
+        x = x.permute(1, 0, 2)  # NLD -> LND
+        x = self.transformer(torch.tensor(x, dtype=torch.float16))
+        x = x.permute(1, 0, 2)  # LND -> NLD
+        x = self.ln_final(x).type(self.dtype)
+
+        # x.shape = [batch_size, n_ctx, transformer.width]
+        # take features from the eot embedding (eot_token is the highest number in each sequence)
+        # x = x[torch.arange(x.shape[0]), tokenized_prompts.argmax(dim=-1)] @ self.text_projection
+        x = x[torch.arange(x.shape[0]), tokenized_prompts.argmax(dim=-1)].float()
+        # x = x @ self.text_projection
+        return x
